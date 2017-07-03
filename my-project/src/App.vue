20709d7d90ba63aed186f687a9b58208d461c4a4
wyan99 <muyanzhi99@gmail.com>
2017-07-03 18:17:36 +0800

    firstVueDemo

---
+++ my-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <Header :logo="logoMsg"></Header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Header from './components/header'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      logoMsg: 'WM'
+    } 
+  },
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-project/src/components/header.vue
@@ -0,0 +1,28 @@
+<template>
+	<header class="header">
+		<div id="logo">{{logo}}</div>
+		<ul class="nav">
+			<li v-for="nav in navs">{{nav.li}}</li>
+		</ul>
+	</header>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+  data () {
+      return {
+        navs: [
+          {li: '主页'},
+          {li: '日志'},
+          {li: '说说'},
+          {li: '主页'},
+          {li: '相册'}
+      ]
+    }
+  },
+  props: ['logo']
+}
+
+</script>
